1e7a28e44363ccabf2faf70363401345c5f5e06b
coldTOUFU <coldtoufu.dev@gmail.com>
2022-09-05 23:23:06 +0900

    add: testのガワだけ追加。

---
+++ test/src/uecda_state_test.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+#include "../../src/uecda_cpp/cards.hpp"
+#include "../../src/uecda_cpp/hand.hpp"
+#include "../../src/uecda_cpp/table.hpp"
+#include "../../src/uecda_cpp/uecda_common.hpp"
+#include "../../src/game_record.hpp"
+#include "../../src/uecda_state.hpp"
+
+using namespace uecda;
+
+TEST(NextTest, Pass) {
+}
+
+TEST(NextTest, PassAtLast) {
+}
+
+TEST(NextTest, IllegalSubmission) {
+}
+
+TEST(NextTest, IllegalSubmissionAtLast) {
+}
+
+TEST(NextTest, NormalSubmission) {
+}
+
+TEST(NextTest, NormalSubmissionWithJoker) {
+}
+
+TEST(NextTest, SubmissionAndGettingOut) {
+}
+
+TEST(NextTest, SubmissionAndLock) {
+}
+
+TEST(NextTest, SubmissionAndRevolution) {
+}
+
+TEST(NextTest, Submission8GiriSingle) {
+}
+
+TEST(NextTest, Submission8GiriPair) {
+}
+
+TEST(NextTest, Submission8GiriSequence) {
+}
+
+TEST(NextTest, SubmissionSpade3GaeshiSingle) {
+}
+
+TEST(NextTest, SubmissionSpade3GaeshiPair) {
+}
+
+TEST(NextTest, SubmissionSpade3GaeshiSequence) {
+}
+
+TEST(LegalActionsTest, Normal) {
+}
+
+TEST(IsFinished, AllPlayersAreNotOut) {
+}
+
+TEST(IsFinished, SomePlayersAreOut) {
+}
+
+TEST(IsFinished, AllPlayersAreOut) {
+}
+
+TEST(GetScore, Daifugo) {
+}
+
+TEST(GetScore, Fugo) {
+}
+
+TEST(GetScore, Heimin) {
+}
+
+TEST(GetScore, Hinmin) {
+}
+
+TEST(GetScore, Daihinmin) {
+}
+
+TEST(GetScore, Undetermined) {
+}
+
+TEST(GetLastAction, Normal) {
+}
+
