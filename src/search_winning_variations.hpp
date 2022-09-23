2dad40496c62715a85430f79de73ba7d893389a1
coldTOUFU <coldtoufu.dev@gmail.com>
2022-09-23 21:50:01 +0900

    change: 必勝手をそのまま返す方が都合がいい(というか必勝手順を返されてもいらない)ので、必勝手だけ返すようにした。

--- src/search_winning_variations.hpp
+++ src/search_winning_variations.hpp
@@ -13,10 +13,7 @@ bool someoneHasNCards(const int n, const uecda::Table& table, const GameRecord& 
 /* trump(切札: 確実に場を流せる)か？ */
 bool is_trump(const uecda::Hand& hand, uecda::Table table, const GameRecord& record, const uecda::Hand& table_hand, const uecda::Cards& cards_of_opponents);
 
-/* 切札をvariation(手順)の先頭にくっつけていく。行きがけDFSなので、探索が終わったときのvariationは先頭から時系列順になる。返り値は成否。 */
-bool pushTrump(const uecda::Cards& my_cards, const uecda::Table& table, const GameRecord& record, const uecda::Hand& table_hand, const uecda::Cards& cards_of_opponents, std::vector<uecda::Hand>& variation);
-
-/* 必勝手順を返す。手は添字順。場を流すときのパスは省略している。必勝手順がない場合は空のベクターを返す。 */
-std::vector<uecda::Hand> searchWinningVariation(const uecda::Cards& my_cards, uecda::Table& table, const GameRecord& record, const uecda::Hand& table_hand, const uecda::Cards& cards_of_opponents);
+/* 必勝手を探索する。あればその手を返し、なければ空の手札を返す。 */
+uecda::Hand winningHand(const uecda::Cards& my_cards, const uecda::Table& table, const GameRecord& record, const uecda::Hand& table_hand, const uecda::Cards& cards_of_opponents);
 
 #endif // SEARCH_WINNING_VARIATIONS_HPP_
